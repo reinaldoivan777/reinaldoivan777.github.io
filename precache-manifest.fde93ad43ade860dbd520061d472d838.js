231e932fcf778bef3a32d4f9d07d23b8ed40734a
Ivan <reinaldoivan777@gmail.com>
2020-01-20 00:42:44 +0700

    first commit

---
+++ precache-manifest.fde93ad43ade860dbd520061d472d838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b2fb625dedeefcf260a4a4191ba860",
+    "url": "reinaldoivan777.github.io/index.html"
+  },
+  {
+    "revision": "a63a56684a2abeb723b9",
+    "url": "reinaldoivan777.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca86810c9da9bea53f50",
+    "url": "reinaldoivan777.github.io/static/js/2.9e9da9f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "reinaldoivan777.github.io/static/js/2.9e9da9f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a63a56684a2abeb723b9",
+    "url": "reinaldoivan777.github.io/static/js/main.eac308ef.chunk.js"
+  },
+  {
+    "revision": "c3a58ce07f5302091489",
+    "url": "reinaldoivan777.github.io/static/js/runtime-main.544620ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "reinaldoivan777.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
