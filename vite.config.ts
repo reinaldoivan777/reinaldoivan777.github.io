1a18f4ee834514c89c3c46564451c411742f455d
ivan <ivan@loyalto.id>
2025-07-17 14:32:07 +0700

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/reinaldoivan777/reinaldoivan777.github.io",
   plugins: [react()],
 });
